9d466f9a4abefebef9a518c00f096e2a6a66f821
Maxim Valov <max-valov@yandex.ru>
2021-03-11 11:12:25 +0300

    +idea

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 14998;
+        int mile = 20;
+        int bonus = price / mile;
+        System.out.println( "Bonus miles:" + bonus);
+    }
+}
